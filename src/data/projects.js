998b1bd9c2630b4201a9de021f4de0992dbd037b
Maria Milkowski <93174242+mmilkowski36@users.noreply.github.com>
2025-03-01 04:10:11 +0000

    Some projects content

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,26 +1,23 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Nearest Dollar',
-    subtitle: '2015 BVP Hackathon',
-    image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
+    title: 'ND Features my Project',
+    subtitle: 'Youth Vaping Poster',
+    link: 'https://news.nd.edu/news/lucy-family-institute-health-department-identify-youth-vaping-intervention-strategies-for-st-joseph-county/',
+    image: '/images/projects/harvest.jpg',
+    date: '2025-02-19',
     desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+      'Notre Dame features my project on technological interventions for '
+      + 'youth vaping, done in collaboration with the Lucy Family Institute and  '
+      + 'the St. Joseph County DoH.',
   },
   {
-    title: 'Harvest',
-    subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
-    link: 'https://devpost.com/software/harvest',
-    image: '/images/projects/harvest.jpg',
-    date: '2015-09-20',
+    title: 'ACM GROUP 2025',
+    subtitle: 'Youth Vaping Poster',
+    image: '/images/projects/nearestdollar.jpg',
+    date: '2025-01-14',
     desc:
-      'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
-      + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
-      + 'and nutrient deficiencies at an affordable price for the developing world.',
+      'Presented Technological Interventions for Youth Vaping Poster at ACM GROUP 2025! ',
   },
   {
     title: 'Space Potato',
